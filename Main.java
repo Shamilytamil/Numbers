82763454d39843f45b76198619010fe117493ee8
Shamilytamil <113530046+Shamilytamil@users.noreply.github.com>
2022-09-24 23:04:54 +0800

    Add files via upload
    Next File

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
-package numbersTest.java;
+package com.Number.java;
 
 import java.util.Scanner;
 
 public class Main {
 	public static void main (String[] args) {
 		
-		 int sNumber,eNumber,len=0;
+		 int sNumber,eNumber,len;
 		 short eveLen=0,oddLen=0;
 		
 		Scanner in = new Scanner(System.in);
@@ -15,11 +15,11 @@ public class Main {
 		System.out.println("Enter your End number must be greater than start Number:\n");
 		eNumber = in.nextInt();
 		
-	
+		len = eNumber-sNumber;
+		
 		for(int i = sNumber;i<=eNumber;i++)
 		{
 		System.out.println(i + ", ");
-		len++;
 		
 		if(i%2 == 0)
 		{
@@ -30,7 +30,6 @@ public class Main {
 		}
 		
 		}
-		len = len-2;
 		System.out.println("\nNumbers between start to end is:" + len );
 		System.out.println("\nNumber of even numbers is :" + eveLen );
 		System.out.println("\nNumber of odd numbers is :" + oddLen );
